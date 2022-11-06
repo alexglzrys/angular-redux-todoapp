cd2350c2eff08bd8ab64d5f062d9d458b5bb08f7
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-06 12:36:14 -0600

    controlar los TODO items

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Todo } from '../models/todo.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() todo!: Todo
+  // Referencia al control de formulario de edición para establecer el foco al momento de editar
+  @ViewChild('inputEdit') inputEdit!: ElementRef;
+
+  chkCompletado!: FormControl;
+  txtEditInput!: FormControl;
+
+  editando: boolean = false;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    // Configurar controles de formulario reactivos con base al estado del todo actual
+    this.chkCompletado = new FormControl(this.todo.completado);
+    this.txtEditInput = new FormControl(this.todo.texto, Validators.required);
+  }
+
+  editar() {
+    this.editando = true;
+    setTimeout(() => {
+      // Seleccionar el contenido del elemento para una facil edición
+      this.inputEdit.nativeElement.select();
+    }, 1);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
   }
 
 }
