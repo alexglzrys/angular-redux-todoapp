c552f2be018ad51bf95798854f5fec98ce77ec34
Alejandro González Reyes <alexglzrys@gmail.com>
2022-11-06 15:08:24 -0600

    conteo de tareas pendientes

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -13,12 +13,26 @@ export class TodoFooterComponent implements OnInit {
 
   filtroActual: filtrosValidos = 'todos';
   filtros: filtrosValidos[] = ['todos', 'completados', 'pendientes'];
+  pendientes: number = 0;
 
   constructor(private store: Store<AppSate>) { }
 
   ngOnInit(): void {
     // Suscribirme y reaccionar si el filtro cambia
-    this.store.select('filtro').subscribe(filtro => this.filtroActual = filtro);
+    // this.store.select('filtro').subscribe(filtro => this.filtroActual = filtro);
+
+    /**
+     * * La ventaja del patrón REDUX es que no necesito preocuparme dónde se dispara la acción que ocasiona que el estado cambie
+     * * Solo es cuestion de suscribirme y en automático me notifica que ha cambiado para reaccionar en consecuencia.
+     */
+
+    // Necesito suscribirme y estar atento a todo el store para reaccionar a ciertos cambios
+    this.store.subscribe(store => {
+      // Conocer que filtro está activado en este momento
+      this.filtroActual = store.filtro;
+      // Conocer cuantas tareas pendientes tengo
+      this.pendientes = store.todos.filter(todo => !todo.completado).length
+    })
   }
 
   cambiarFiltro(nuevo_filtro: filtrosValidos) {
